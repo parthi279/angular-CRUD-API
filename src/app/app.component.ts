3fd8c7528010da737be2eaeb560c9616a35dcbe1
parthi279 <parthi279@gmail.com>
2021-10-18 19:18:07 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular hi ' + VERSION.major;
+  name = 'Angular hi jbl' + VERSION.major;
 }
