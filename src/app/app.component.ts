decf9f7b42561238284fef271914210406a51407
parthi279 <parthi279@gmail.com>
2021-12-19 07:53:08 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, NgModule, VERSION } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular hi jbl' + VERSION.major;
+
+export class AppComponent {
+
+  registrationForm = new FormGroup({
+    userName: new FormControl("",)
+  });
+
+  onSubmit() {}
 }
